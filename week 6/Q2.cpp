79c86c0540e99f122e4fec735d3e84b32c1d1ca8
tanishkchauhan121 <158290342+tanishkchauhan121@users.noreply.github.com>
2024-05-05 10:13:02 +0530

    Create Q2.cpp

---
+++ week 6/Q2.cpp
@@ -0,0 +1,57 @@
+#include<bits/stdc++.h>
+using namespace std;
+bool bfs(int src,int col,int v,vector<vector<int>>&adj)
+{  
+  vector<int> vis(v,-1);
+  vis[src]=col;
+  queue<int>q;
+  q.push(src);
+  while(!q.empty())
+  {
+    int node=q.front();
+    q.pop();
+    for(auto it :adj[node])
+    { 
+      if(vis[it]==-1)
+      {
+        q.push(it);
+        vis[it]=!(vis[node]);
+        
+      }
+      else
+      {
+        if(vis[it]==vis[node])
+            return false;
+      }
+
+    }
+
+  }
+  return true;
+}
+
+int main()
+{
+    int e,v;
+    cout<<"Enter no of nodes";
+    cin>>v;
+    cout<<"Enter edges";
+    cin>>e;
+    vector<vector<int>>adj(v);
+    for(int i=0;i<e;i++)
+    {
+       int u,v;
+    cin>>u>>v;
+    adj[u].push_back(v);
+    adj[v].push_back(u);
+    }
+    bool res = bfs(0,0,v,adj);
+    
+    if(res==1)
+    {
+        cout<<"true";
+    }
+    else 
+      cout<<"false";
+
+}
